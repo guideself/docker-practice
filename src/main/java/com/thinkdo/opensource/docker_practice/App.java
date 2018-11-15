e28b7ecd83940d66af9b2b8ea7a4af9522eb4a8b
guideself <welcomzs@sina.com>
2018-11-15 19:37:18 +0800

    docker-practice submit

---
+++ src/main/java/com/thinkdo/opensource/docker_practice/App.java
@@ -0,0 +1,13 @@
+package com.thinkdo.opensource.docker_practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
